ca269456d59663502b29df5c773783e4bab49bc6
jkciesluk <323892@uwr.edu.pl>
2021-01-03 15:19:39 +0100

    Prototype function for reading records

--- time_records.c
+++ time_records.c
@@ -9,5 +9,26 @@ time_t game_time(time_t t1, time_t t2){
 }
 
 void print_time(time_t t){
-    printf("%.2ld:%.2ld\n", t/60, t%60);
+    printf("%.2ld:%.2ld:%.2ld\n",t/3600, t/60, t%60);
+}
+
+int conv(const char *c){
+    int res=0;
+    while(*c){
+        res*=10;
+        res+=*c-'0';
+        c++;
+    }
+    return res;
+}
+
+int get_records(){
+    char c[10][5];
+    FILE *plik;
+    plik=fopen("records.txt", "r");
+    for(int i=0; i<10; i++){
+        fgets(char[i], 5, plik);
+        printf("%d ", conv(c[i]));       
+    }
+    return 1;
 }
--- time_records.h
+++ time_records.h
@@ -5,3 +5,5 @@
 time_t get_time();
 time_t game_time(time_t t1, time_t t2);
 void print_time(time_t t);
+int conv(const char *c);
+int get_records();
